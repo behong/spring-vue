f49649339fd636bdff160d4b2da3b3988137b4a3
WELSH_CORGI_PC\user <sijm@naver.com>
2023-04-22 16:35:19 +0900

    thymeleaf 화면단 list 처리 예제

--- src/main/java/org/duckdns/jisou/controller/SampleController.java
+++ src/main/java/org/duckdns/jisou/controller/SampleController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @Log4j2
 public class SampleController{
@@ -14,5 +17,13 @@ public class SampleController{
         log.info("hello....");
 
         model.addAttribute("msg","HELLO WORLD");
+    }
+
+    @GetMapping("/ex/ex1")
+    public void ex1(Model model){
+
+        List<String> list = Arrays.asList("AAA", "BBB", "CCC");
+
+        model.addAttribute("list",list);
     }
 }
