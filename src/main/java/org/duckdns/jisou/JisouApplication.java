528acf38800aae43eec35f750489dabbc4008ff2
WELSH_CORGI_PC\user <sijm@naver.com>
2023-04-22 15:47:47 +0900

    프로젝트 초기 생성 230422

---
+++ src/main/java/org/duckdns/jisou/JisouApplication.java
@@ -0,0 +1,13 @@
+package org.duckdns.jisou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JisouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JisouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/duckdns/jisou/JisouApplicationTests.java
@@ -0,0 +1,13 @@
+package org.duckdns.jisou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JisouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
